abfa1ef22cff24ec80ccf97fbe7e6cdcd6894999
JancikK <yonaskollegaev@gmail.com>
2025-06-17 20:54:35 +0300

    test created, replaced

--- Bash_scripts/Lesson21/test.sh
+++ Bash_scripts/Lesson21/test.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo "Running test.sh script..."
+echo "Running Docker container test..."
+
+# Check SSH service exists
+if ! command -v sshd >/dev/null; then
+  echo "sshd is not installed"
+  exit 1
+fi
+
+# Optionally check user exists
+if ! id ansible &>/dev/null; then
+  echo "user 'ansible' does not exist"
+  exit 1
+fi
+
+echo "All checks passed"
+exit 0
